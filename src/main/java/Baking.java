9e9f3ea1cf9868feab1dce8f7d50ee54271bf951
rimmderes <rimmderes@gmail.com>
2022-11-21 14:50:50 +0000

    Added classes and getters and setters

---
+++ src/main/java/Baking.java
@@ -0,0 +1,27 @@
+public abstract class Baking {
+
+    private String name;
+    private String baseFlavour;
+
+    public Baking(String name, String baseFlavour) {
+        this.name = name;
+        this.baseFlavour = baseFlavour;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBaseFlavour() {
+        return this.name;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/TieredCakes.java
@@ -0,0 +1,2 @@
+public class TieredCakes {
+}
